37057d2a61616b4601e4f713e3ca870a1f75a314
mc-nakayama <michitoshi.nakayama@gmail.com>
2020-09-12 22:17:14 +0900

    アクションとルーティング

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+ before_action :set_param only: [:show, :edit]
+
+
   def index
     @user = current_user
     @prototypes = Prototype.all
@@ -17,9 +20,18 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def show
+  end
+
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept,:image).merge(user_id: current_user.id)
   end
+
+  def set_param
+    @prototype = Prototype.find(params[:id])
+  end
+
 
 end
